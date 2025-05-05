76c0b470b48065932cb563e880ea5ec14cb55076
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-05-05 14:28:52 -0300

    fix: fix gluestack themed

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { Text, View, StatusBar } from 'react-native'
+import { Text, Center, GluestackUIProvider } from '@gluestack-ui/themed'
+import { View, StatusBar } from 'react-native'
 import './src/global.css'
+
+import { Loading } from '@components/Loading'
 
 export default function App() {
   return (
-    <View className=' flex-1 flex justify-center items-center bg-zinc-900'>
-      <Text> Handly </Text>
-      <StatusBar className='color-white' translucent backgroundColor='transparent' />
-    </View>
+    <GluestackUIProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Center className="bg-stone-900 flex flex-1 justify-center items-center">
+        <Text className=" text-stone-100"> Handly </Text>
+      </Center>
+    </GluestackUIProvider>
   )
 }
-
