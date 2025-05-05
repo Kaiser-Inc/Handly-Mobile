4208b0808da7891084a33d0c987edb7d8062127c
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-05-05 14:41:09 -0300

    feat(loading): add loading component

--- App.tsx
+++ App.tsx
@@ -2,9 +2,16 @@ import { Text, Center, GluestackUIProvider } from '@gluestack-ui/themed'
 import { View, StatusBar } from 'react-native'
 import './src/global.css'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
 import { Loading } from '@components/Loading'
 
 export default function App() {
+  const [fonstsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <GluestackUIProvider>
       <StatusBar
@@ -12,9 +19,13 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Center className="bg-stone-900 flex flex-1 justify-center items-center">
-        <Text className=" text-stone-100"> Handly </Text>
-      </Center>
+      {fonstsLoaded ? (
+        <Center className="bg-stone-900 flex flex-1 justify-center items-center">
+          <Text className=" text-stone-100 text-3xl font-normal"> Handly </Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
     </GluestackUIProvider>
   )
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import { Center, Spinner } from '@gluestack-ui/themed'
 
 export function Loading() {
   return (
-    <Center flex={1}>
-      <Spinner />
+    <Center className=" bg-stone-900 flex flex-1 justify-center items-center">
+      <Spinner className=" text-stone-700" size="large" />
     </Center>
   )
 }
