080d5c174f2f7eed3f303503ca1db9b03130fe8b
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-05-15 08:25:34 -0300

    feat(components): add form input component

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -3,15 +3,7 @@ import {
   Image,
   VStack,
   Text,
-  Link,
-  LinkText,
   FormControl,
-  Input,
-  InputField,
-  InputSlot,
-  InputIcon,
-  EyeIcon,
-  EyeOffIcon,
   Button,
   ButtonText,
   HStack,
@@ -27,21 +19,22 @@ import React from 'react'
 
 import { z } from 'zod'
 
-import { useForm, Controller } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { createUser } from '@services/api/users-services'
 import { useNavigation } from '@react-navigation/native'
 import type { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 import { GradientButton } from '@components/GradientButton'
+import { FormInput } from '@components/FormInput'
 
 const signUpSchema = z.object({
-  name: z.string({ required_error: 'Campo Obrigatório' }),
+  name: z.string({ required_error: 'Campo obrigatório' }),
   email: z
-    .string({ required_error: 'Campo Obrigatório' })
+    .string({ required_error: 'Campo obrigatório' })
     .email('Email invalido'),
   password: z
-    .string({ required_error: 'Campo Obrigatório' })
+    .string({ required_error: 'Campo obrigatório' })
     .min(8, 'A senha deve conter pelo menos 8 caracteres')
     .refine((val) => /[a-zA-Z]/.test(val), {
       message: 'A senha deve conter pelo menos uma letra',
@@ -112,88 +105,25 @@ export function SignUp() {
             </Center>
             <Center className=" bg-white flex flex-col flex-1 rounded-tr-3xl rounded-tl-3xl pt-12 items-center ">
               <Logo />
-              <FormControl className=" w-full h-fit flex">
-                <VStack className=" w-full px-8 mt-12">
-                  <Text className="text-xl font-bold mb-2">
-                    {' '}
-                    Nome Completo{' '}
-                  </Text>
-                  <Controller
-                    control={control}
-                    name="name"
-                    render={({ field: { onChange, value } }) => (
-                      <Input className="">
-                        <InputField
-                          value={value}
-                          onChangeText={onChange}
-                          className={`text-base border ${errors.name ? 'border-danger-300' : 'border-purple-300'} rounded-lg h-16 mb-3`}
-                        />
-                      </Input>
-                    )}
-                  />
-                  {errors.name && (
-                    <Text className="text-danger-300">
-                      {errors.name.message}{' '}
-                    </Text>
-                  )}
-                </VStack>
-                <VStack className=" w-full px-8 mt-4">
-                  <Text className="text-xl font-bold mb-2"> Email </Text>
-                  <Controller
-                    control={control}
-                    name="email"
-                    render={({ field: { onChange, value } }) => (
-                      <Input className="">
-                        <InputField
-                          autoCapitalize="none"
-                          value={value}
-                          onChangeText={onChange}
-                          className={`text-base border ${errors.email ? 'border-danger-300' : 'border-purple-300'} rounded-lg h-16 mb-3`}
-                        />
-                      </Input>
-                    )}
-                  />
-                  {errors.email && (
-                    <Text className="text-danger-300">
-                      {errors.email.message}{' '}
-                    </Text>
-                  )}
-                </VStack>
-                <VStack className=" w-full px-8 mt-4">
-                  <Text className="text-xl font-bold mb-2"> Senha </Text>
-                  <Controller
-                    control={control}
-                    name="password"
-                    render={({ field: { onChange, value } }) => (
-                      <Input className="">
-                        <InputField
-                          autoCapitalize="none"
-                          value={value}
-                          onChangeText={onChange}
-                          type={showPassword ? 'password' : 'text'}
-                          className={`text-base border ${errors.password ? 'border-danger-300' : 'border-purple-300'} rounded-lg h-16`}
-                        />
-
-                        <InputSlot
-                          className="ml-auto -mt-12 mr-4 h-16"
-                          onPress={handleState}
-                        >
-                          <InputIcon
-                            as={showPassword ? EyeOffIcon : EyeIcon}
-                            width={28}
-                            height={30}
-                            color="#CEBDF2"
-                          />
-                        </InputSlot>
-                      </Input>
-                    )}
-                  />
-                  {errors.password && (
-                    <Text className="text-danger-300">
-                      {errors.password.message}{' '}
-                    </Text>
-                  )}
-                </VStack>
+              <FormControl className=" w-full h-fit flex mt-8">
+                <FormInput
+                  control={control}
+                  name="name"
+                  label="Nome Completo"
+                  error={errors.name?.message}
+                />
+                <FormInput
+                  control={control}
+                  name="email"
+                  label="Email"
+                  error={errors.email?.message}
+                />
+                <FormInput
+                  control={control}
+                  name="password"
+                  label="Senha"
+                  error={errors.password?.message}
+                />
 
                 <GradientButton
                   onPress={handleSubmit(handleOnSubmit)}
