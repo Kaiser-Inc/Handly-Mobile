e1e3b2dcfeb68023488b9155367abf6d6d974a04
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-06-05 22:20:53 -0300

    feat: remove signOut from feed

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -1,19 +1,15 @@
 import {
   Image,
   ScrollView,
-  Text,
   VStack,
 } from '@gluestack-ui/themed'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import BackgroundImg from '@assets/bg.png'
-import { GradientButton } from '@components/GradientButton'
 import { HomeHeader } from '@components/HomeHeader'
 import { SearchBar } from '@components/SearchBar'
-import { useAuth } from '@hooks/useAuth'
 
 export function Feed() {
-  const { signOut } = useAuth()
   return (
     <SafeAreaView className="flex-1" style={{ backgroundColor: 'transparent' }}>
       <Image
@@ -30,7 +26,6 @@ export function Feed() {
         className="flex bg-white flex-col rounded-tr-3xl rounded-tl-3xl pt-10"
         showsVerticalScrollIndicator={false}
       >
-        <GradientButton text="Sair" onPress={signOut} />
       </ScrollView>
     </SafeAreaView>
   )
