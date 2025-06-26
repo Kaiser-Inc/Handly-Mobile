9f519422c6598d34ff0033147cb08bc816f53695
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-06-26 16:23:45 -0300

    feat(edit profile): add profile edit components

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -40,6 +40,7 @@ export function AppRoutes() {
           borderTopWidth: 0,
           height: Platform.OS === 'android' ? 75 : 90,
           paddingTop: 12,
+          
         },
         animation: 'fade',
       }}
