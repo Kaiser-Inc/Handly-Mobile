fa6079bab770222b841c2a2bd4ca532a4b0b5b33
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-07-13 11:38:55 -0300

    feat(search): add search bar feature on favorites

--- src/screens/Favorites.tsx
+++ src/screens/Favorites.tsx
@@ -4,12 +4,15 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import BackgroundImg from '@assets/bg.png'
 import { HomeHeader } from '@components/HomeHeader'
 import { Post } from '@components/Post'
+import { SearchBar } from '@components/SearchBar'
 import type { ServiceFeedDTO } from '@dtos/serviceDTO'
 import { fetchServices, getFeed } from '@services/services-services'
 import { useEffect, useState } from 'react'
 
 export function Favorites() {
   const [services, setServices] = useState([])
+  const [search, setSearch] = useState('')
+
   useEffect(() => {
     async function loadServices() {
       const data = await getFeed()
@@ -17,6 +20,12 @@ export function Favorites() {
     }
     loadServices()
   }, [])
+
+  const filteredServices = services.filter(
+    (service: ServiceFeedDTO) =>
+      service.service_name.toLowerCase().includes(search.toLowerCase()) ||
+      service.description?.toLowerCase().includes(search.toLowerCase()),
+  )
 
   return (
     <SafeAreaView className="flex-1" style={{ backgroundColor: 'transparent' }}>
@@ -27,13 +36,15 @@ export function Favorites() {
         className="w-full h-full absolute"
       />
       <VStack className="flex">
-        <HomeHeader />
+        <HomeHeader>
+          <SearchBar onChange={setSearch} />
+        </HomeHeader>
       </VStack>
       <ScrollView
         className="flex bg-white flex-col rounded-tr-3xl rounded-tl-3xl pt-10"
         showsVerticalScrollIndicator={false}
       >
-        {services.map((service: ServiceFeedDTO) => (
+        {filteredServices.map((service: ServiceFeedDTO) => (
           <Post
             key={service.service_name}
             name={service.service_name}
