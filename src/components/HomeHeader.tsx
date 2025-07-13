1914a664fbb25c6a7f46e2f1a73c4757985bbbb1
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-07-13 11:36:38 -0300

    feat(search): add search bar feature on feed

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,17 +1,19 @@
 import { HStack } from '@gluestack-ui/themed'
 import { Bell, House } from 'lucide-react-native'
-import { SearchBar } from './SearchBar'
+import type { ReactNode } from 'react'
 
-export function HomeHeader() {
+interface HomeHeaderProps {
+  children?: ReactNode
+}
+
+export function HomeHeader({ children }: HomeHeaderProps) {
   return (
-    <HStack className=" mx-auto rounded-2xl w-11/12 flex flex-row px-8 bg-white mb-8">
-      <HStack className=" flex flex-row items-center ">
+    <HStack className="mx-auto rounded-2xl w-11/12 flex flex-row px-8 bg-white mb-8 items-center">
+      <HStack className="flex flex-row items-center">
         <House size={30} stroke="#9356FC" />
       </HStack>
-      <HStack className="flex flex-row flex-1">
-        <SearchBar />
-      </HStack>
-      <HStack className=" flex flex-row items-center gap-4">
+      <HStack className="flex flex-row flex-1">{children}</HStack>
+      <HStack className="flex flex-row items-center gap-4">
         <Bell size={30} />
       </HStack>
     </HStack>
